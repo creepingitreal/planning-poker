c550e058fc1c3346f62309fe1567839c0d6e1c40
Joanne Demetriades <joannedemetriades@joannes-mbp.lan>
2025-05-13 19:10:57 +0100

    Add room.vue files

--- client/src/components/Room.vue
+++ client/src/components/Room.vue
@@ -1,15 +1,42 @@
 <template>
     <div class="room">
-       <h1>Room: {{roomId}}</h1>
+       <h1>Room: {{ roomId }}</h1>
+       <h2>Welcome, {{ name }}!</h2>
        <div class="players">
          <h2>Players</h2>
          <ul>
-           <li v-for="player in players" :key="player.id">{{ player.name }}</li>
+           <li v-for="player in players" :key="player.id">{{ player.user }}</li>
+           <pre>{{ players }}</pre>
          </ul>
        </div>
-   
+       <div class="game-board">
+         the game board will go here
+       </div>
     </div>
-</template>
-
-<script setup>
-</script>
+   </template>
+   
+   <script setup>
+   const props = defineProps({
+       roomId: {
+           type: String,
+           required: true
+       },
+       players: {
+           type: Object,
+           default: () => [],
+           required: true
+       },
+       name: {
+           type: String,
+           required: true
+       }
+   });
+   </script>
+   
+   <style scoped>
+   .game-board {
+       border: 1px solid #fff;
+       padding: 100px;
+       
+   }
+   </style>
