1c15a7361e3eb5397c4e6ba6fbcdb376b16bd472
Alexandra <alex.demetriades@procode.co.uk>
2025-06-10 20:31:03 +0100

    add dockerfiles, docker-compose file

---
+++ server/server.js
@@ -0,0 +1,73 @@
+// server/server.js (converted to ES Module syntax)
+import express from 'express';
+import http from 'http';
+import Server from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('joinRoom', ({ roomId, user }) => {
+        socket.join(roomId);
+
+        if (!rooms[roomId]) rooms[roomId] = {};
+
+        rooms[roomId][socket.id] = { user, vote: null };
+
+        io.to(roomId).emit('updateRoom', rooms[roomId]);
+        console.log('📢 Joining room, roomUpdate:', rooms[roomId])
+    });
+
+    socket.on('castVote', ({ roomId, vote }) => {
+        if (rooms[roomId] && rooms[roomId][socket.id]) {
+            rooms[roomId][socket.id].vote = vote;
+            console.log('Vote updated', rooms[roomId][socket.id]);
+        }
+        io.to(roomId).emit('updateRoom', rooms[roomId]);
+        console.log('📢 Vote cast, roomUpdate:', rooms[roomId]);
+    });
+    
+
+    socket.on('revealVotes', (roomId) => {
+        io.to(roomId).emit('revealVotes');
+    });
+
+    socket.on('resetVotes', (roomId) => {
+        Object.keys(rooms[roomId] || {}).forEach((userId) => {
+            rooms[roomId][userId].vote = null;
+        });
+        io.to(roomId).emit('updateRoom', rooms[roomId]);
+    }); 
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+        for (const roomId in rooms) {
+            if (rooms[roomId][socket.id]) {
+                delete rooms[roomId][socket.id];
+                io.to(roomId).emit('updateRoom', rooms[roomId]);
+            }
+        }
+    });
+
+    socket.on('error', (error) => {
+        console.error('Socket error:', error);
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
