08b97f4f43c4ca38c1be162712aaa0b67e047037
Alexandra <alex.demetriades@procode.co.uk>
2025-10-08 18:32:52 +0100

    Add votebutton component

---
+++ client-react/src/pages/components/VoteButtons.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import socket from "../../socket.ts";
+
+const voteOptions: any[] = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, '?'];
+
+const VoteButtons:
+    React.FC = () => {
+    const castVote = (value: number | string) => {
+        console.log("VOTED", roomId, value);
+        socket.emit('castVote', { roomId, vote: value});
+    };
+
+    return (
+        <div>
+            {voteOptions.map(option => (
+                <button key={ String(option) } onClick={() => castVote(option)}>{String(option)}</button>
+            )) }
+        </div>
+    );
+};
+
+export default VoteButtons;
