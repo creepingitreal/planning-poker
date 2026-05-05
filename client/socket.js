7e5ff892e5bd0b6c853fb5a3a46054e596581f70
Alexandra <alex.demetriades@procode.co.uk>
2026-05-05 17:59:31 +0100

    Styling and favicon

--- client/socket.js
+++ client/socket.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io(
-    import.meta.env.VITE_SERVER_URL ?? globalThis.location.origin
-);
+const isLocalHost = ['localhost', '127.0.0.1'].includes(globalThis.location.hostname);
+const fallbackUrl = isLocalHost ? 'http://localhost:8000' : globalThis.location.origin;
+
+const socket = io(import.meta.env.VITE_SERVER_URL ?? 'http://localhost:8000');
 
 export default socket;
