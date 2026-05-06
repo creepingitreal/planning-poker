9e72ecf1ea24f253dd573f014b67da85c38cac4c
Alexandra <alex.demetriades@procode.co.uk>
2026-05-06 11:13:48 +0100

    fix socket.js

--- client/socket.js
+++ client/socket.js
@@ -3,6 +3,6 @@ import io from 'socket.io-client';
 const isLocalHost = ['localhost', '127.0.0.1'].includes(globalThis.location.hostname);
 const fallbackUrl = isLocalHost ? 'http://localhost:8000' : globalThis.location.origin;
 
-const socket = io(import.meta.env.VITE_SERVER_URL ?? 'http://localhost:8000');
+const socket = io(import.meta.env.VITE_SERVER_URL ?? fallbackUrl);
 
 export default socket;
